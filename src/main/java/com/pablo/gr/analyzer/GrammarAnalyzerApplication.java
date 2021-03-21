b40096d4b4b7181757f3411fe9d3ad7a58f89243
p-jacobo2012240 <pablojacobo1998@gmail.com>
2021-03-20 21:22:20 -0600

    init parser project

---
+++ src/main/java/com/pablo/gr/analyzer/GrammarAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.pablo.gr.analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrammarAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrammarAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pablo/gr/analyzer/GrammarAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablo.gr.analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrammarAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
