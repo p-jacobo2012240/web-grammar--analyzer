e863ae5f5b0d61ced70d7b736eb26bdd4abde486
p-jacobo2012240 <pablojacobo1998@gmail.com>
2021-03-20 21:38:48 -0600

    bind html to controller

--- src/main/java/com/pablo/gr/analyzer/controllers/IndexController.java
+++ src/main/java/com/pablo/gr/analyzer/controllers/IndexController.java
@@ -1,5 +1,14 @@
 package com.pablo.gr.analyzer.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class IndexController {
 
+	@GetMapping({"/index", "/"})
+	public String index() {
+		return "index.html";
+	}
+	
 }
