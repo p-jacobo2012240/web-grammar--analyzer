efb67e71b2a9267bb347480d79609b822fe675e6
p-jacobo2012240 <pablojacobo1998@gmail.com>
2021-03-21 18:13:49 -0600

    add endpoint change txt file

---
+++ src/main/java/com/pablo/gr/analyzer/controllers/AnalyzerController.java
@@ -0,0 +1,27 @@
+package com.pablo.gr.analyzer.controllers;
+
+import java.io.IOException;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+public class AnalyzerController {
+
+	@PostMapping("/upload/file")
+	public ResponseEntity<Object> uploadFileTxT(@RequestParam("filetxt") MultipartFile fileTxt  ){
+		if(!fileTxt.isEmpty()) {
+			try {
+				System.out.println(" content "+  fileTxt.getInputStream().toString());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+	
+}
