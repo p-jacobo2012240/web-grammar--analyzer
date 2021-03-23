a736dbb2ae87563a13040089f59dbcd4448855da
p-jacobo2012240 <pablojacobo1998@gmail.com>
2021-03-23 12:32:49 -0600

    fix issue headers into vue controller

--- src/main/java/com/pablo/gr/analyzer/controllers/AnalyzerController.java
+++ src/main/java/com/pablo/gr/analyzer/controllers/AnalyzerController.java
@@ -7,32 +7,37 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+
 @RestController
 public class AnalyzerController {
-
+	
+	private Logger logger = LoggerFactory.getLogger(AnalyzerController.class);
+	
 	@PostMapping("/upload/file")
-	public ResponseEntity<Object> uploadFileTxT(@RequestParam("filetxt") MultipartFile fileTxt) {
+	public ResponseEntity<Object> uploadFileTxT(@RequestParam("file") MultipartFile fileTxt) {
 		if (!fileTxt.isEmpty()) {
 			BufferedReader bufferedReader;
 			List<String> resultList = new ArrayList<>();
+			String line = "";
 			try {
-				String line;
 				InputStream inputStream = fileTxt.getInputStream();
 				bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
 				// setting data into string list
 				while ((line = bufferedReader.readLine()) != null) {
 					resultList.add(line);
-				}
+				}  
 				
 				// read line...
 				for(String letter: resultList) {
-					System.out.println("line " + letter );
+					this.logger.info("line -- > " + letter );   
 				}
 
 			} catch (IOException e) {
--- src/main/resources/static/vue-controller.js
+++ src/main/resources/static/vue-controller.js
@@ -12,26 +12,17 @@ var app = new Vue({
 		},
 		submitFile() {
 			let formData = new FormData();
-			// formData.append('filetxt', this.file);
-			
-			
-
-			//console.log('logs ', formData ); // key1 = value1, then key2 = value2
-		
-
+			formData.append('file', this.$refs.file.files[0]);
 
 			//call api
-			/*fetch('http://localhost:8080/upload/file', {
+			fetch('http://localhost:8080/upload/file', {
 				method: 'POST',
-				headers: {
-					'Content-Type': 'multipart/form-data',
-				},
 				body: formData
 			})
 				.then(response => response.json())
 				.then(data => {
 					console.log('Success:', data);
-				})*/
+				})
 
 
 
