38905cc4633e288f8b0d765c99eb3c9ffeb49991
p-jacobo2012240 <pablojacobo1998@gmail.com>
2021-04-02 23:39:03 -0600

    [add] model entries txt

---
+++ src/main/java/com/pablo/gr/analyzer/models/RawText.java
@@ -0,0 +1,13 @@
+package com.pablo.gr.analyzer.models;
+
+public class RawText {
+	private String line;
+
+	public String getLine() {
+		return line;
+	}
+
+	public void setLine(String line) {
+		this.line = line;
+	}
+}
