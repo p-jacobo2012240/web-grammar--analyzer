1e59f18c140abbdbb2d89578da0050cc11f66b76
p-jacobo2012240 <pablojacobo1998@gmail.com>
2021-04-02 23:44:21 -0600

    implements productions and terminals models

---
+++ src/main/java/com/pablo/gr/analyzer/models/Productions.java
@@ -0,0 +1,13 @@
+package com.pablo.gr.analyzer.models;
+
+public class Productions {
+	private String expProduction;
+
+	public String getExpProduction() {
+		return expProduction;
+	}
+
+	public void setExpProduction(String expProduction) {
+		this.expProduction = expProduction;
+	}
+}
---
+++ src/main/java/com/pablo/gr/analyzer/models/Terminals.java
@@ -0,0 +1,13 @@
+package com.pablo.gr.analyzer.models;
+
+public class Terminals {
+	private String character;
+
+	public String getCharacter() {
+		return character;
+	}
+
+	public void setCharacter(String character) {
+		this.character = character;
+	}
+}
