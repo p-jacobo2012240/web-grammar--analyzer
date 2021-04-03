a95c907697419250d351cb6f9b32aaa991e803ac
p-jacobo2012240 <pablojacobo1998@gmail.com>
2021-04-02 23:41:47 -0600

    fix 'variables' model

---
+++ src/main/java/com/pablo/gr/analyzer/models/Variables.java
@@ -0,0 +1,15 @@
+package com.pablo.gr.analyzer.models;
+
+public class Variables {
+	private String character;
+
+	public String getCharacter() {
+		return character;
+	}
+
+	public void setCharacter(String character) {
+		this.character = character;
+	}
+	
+	
+}
