4788103458e48f592852c3f1008de1f41d28b603
p-jacobo2012240 <pablojacobo1998@gmail.com>
2021-04-09 15:06:16 -0600

    add characters

--- src/main/java/com/pablo/gr/analyzer/engines/ScanerToken.java
+++ src/main/java/com/pablo/gr/analyzer/engines/ScanerToken.java
@@ -3,9 +3,15 @@ package com.pablo.gr.analyzer.engines;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Stack;
+import java.util.stream.Collectors;
+
+import com.pablo.gr.analyzer.utils.SyntaxCharacters;
 
 public class ScanerToken {
 	private static ScanerToken instance;
+	private Map<String, Object> objByToken = new HashMap<>();
+	private SyntaxCharacters syntaxCharacters = new SyntaxCharacters();
 	
 	public static ScanerToken getInstance() {
 		if (instance == null) {
@@ -13,31 +19,48 @@ public class ScanerToken {
 		}
 		return instance;
 	}
-	
 
 	public Map<String, Object> evaluateToken(List<String> fileStruct) {
 		Map<String, Object> resultEval = new HashMap<>();
-		
-		
-		fileStruct.stream().forEach( strTkn -> {
+
+		fileStruct.stream().forEach(strTkn -> {
 			System.out.println("element feach " + this.splitedToken(strTkn));
 			System.out.println("*************************************");
 		});
-		
-		
+
 		return resultEval;
 	}
-	
+
 	public String splitedToken(String currentToken) {
 		String[] splCurrentToken = currentToken.split("");
-		
-		for(String val : splCurrentToken) {
-			System.out.println("tkn --> "+ val  );
+		Stack<String> stackTokenChar = new Stack<>();
+
+		for (String value : splCurrentToken) {
+			stackTokenChar.add(value);
 		}
 		
+		this.objByToken.put("variable", stackTokenChar.firstElement());
+		
+	
+		List<String> stackFiltered = stackTokenChar
+			.stream()
+			.filter(tkn -> !tkn.equals(stackTokenChar.firstElement()))
+			.filter(tkn -> !tkn.equals(syntaxCharacters.QUOTATION_MARK_SIMPLE))
+			.filter(tkn -> !tkn.equals(syntaxCharacters.EQUAL_SIGN))
+			.filter(tkn -> !tkn.equals(syntaxCharacters.QUOTATION_MARK))
+			.collect(Collectors.toList());
+		
+		
+		for(String xx : stackFiltered ) {
+			System.out.println("valll "+ xx );
+		}
+	
+		
+
+	   
+	
+
 		return " ";
 	}
-	
-	
 
 }
---
+++ src/main/java/com/pablo/gr/analyzer/utils/SyntaxCharacters.java
@@ -0,0 +1,7 @@
+package com.pablo.gr.analyzer.utils;
+
+public class SyntaxCharacters {
+	public static final String QUOTATION_MARK_SIMPLE = "’";
+	public static final String EQUAL_SIGN = "=";
+	public static final String QUOTATION_MARK = "'";
+}
