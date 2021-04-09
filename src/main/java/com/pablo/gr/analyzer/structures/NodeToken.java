0c97d7e593fd27ed7222edbf2940f39dad56eb2e
p-jacobo2012240 <pablojacobo1998@gmail.com>
2021-04-09 00:49:37 -0600

    basics handlers into pile structure

--- src/main/java/com/pablo/gr/analyzer/structures/NodeToken.java
+++ src/main/java/com/pablo/gr/analyzer/structures/NodeToken.java
@@ -1,5 +1,30 @@
 package com.pablo.gr.analyzer.structures;
 
 public class NodeToken {
+	private String valueToken;
+	private NodeToken nextNode;
+	
+	public NodeToken() {
+		this.valueToken = "";
+		this.nextNode = null;
+	}
+
+	public String getValueToken() {
+		return valueToken;
+	}
+
+	public void setValueToken(String valueToken) {
+		this.valueToken = valueToken;
+	}
+
+	public NodeToken getNextNode() {
+		return nextNode;
+	}
+
+	public void setNextNode(NodeToken nextNode) {
+		this.nextNode = nextNode;
+	}
+	
+	
 
 }
